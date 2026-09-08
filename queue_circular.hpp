03bfff544d7df7fa9b9adb5511b0175b1056ae57
Gon512 <gmamaniba@unsa.edu.pe>
2026-09-07 23:09:08 -0500

    Implementacion de la queue circular, la cual es bastante similar a la queue shift con la excepcion del physical_index para poder usar la posicion fisica de los elementos, y con la simplificacion de pop simplemente avanzando una posicion el head

--- queue_circular.hpp
+++ queue_circular.hpp
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef QUEUECIRCULAR
+#define QUEUECIRCULAR
 
 #include <cstddef>
 #include <stdexcept>
@@ -49,29 +50,74 @@ std::size_t QueueCircular<T>::physical_index(std::size_t logical) const noexcept
 
 template<typename T>
 void QueueCircular<T>::grow() {
-  throw std::logic_error("TODO QueueCircular::grow");
+size_t new_capacity_;
+	if(capacity_==0)
+		new_capacity_=1;
+	else new_capacity=capacity_*2;
+	T* new_data=new T[new_capacity_];
+	for(size_t i=0;i<size_;i++){
+		new_data[i]=data_[physical_index(i)];
+		moves_++;}
+	delete[] data_;
+	data_=new_data;
+	capacity_=new_capacity_;
+	head_=0;}
 }
 
 template<typename T>
-QueueCircular<T>::QueueCircular(const QueueCircular &) {
-  throw std::logic_error("TODO QueueCircular copy constructor");
+QueueCircular<T>::QueueCircular(const QueueCircular &other):data_(nullptr),head_(0),size_(other.size_),capacity_(other.capacity_),moves_(0){
+if(capacity_>0){
+	data_=new T[capacity_];
+	for(size_t i=0;i<size_;i++)
+		data_[i]=other.data_[physical_index(i)];}
 }
 
 template<typename T>
-QueueCircular<T>::QueueCircular(QueueCircular &&) noexcept {
-  // TODO: transferir ownership y dejar el origen vacío.
+QueueCircular<T>::QueueCircular(QueueCircular &&other) noexcept:data_(other.data_),head_(other.head_) size_(other.size_), capacity_(other.capacity_), moves_(other.moves_)i{
+	other.data_=nullptr;
+	other.head_=0;
+	other.size_=0;
+	other.capacity_=0;
+	other.moves_=0;
 }
+
 
 template<typename T>
 QueueCircular<T> &QueueCircular<T>::operator=(const QueueCircular &) {
-  throw std::logic_error("TODO QueueCircular copy assignment");
+   if(this!=&other){
+	T* new_data_=nullptr;
+	if(other.capacity_>0){
+		new_data_=new T[other.capacity_];
+		for(size_t i=0;i<other.size_;i++)
+			new_data_[i]=other.data_[other.physical_index(i)];
+			}
+	delete[] data_;
+	
+	data_=new_data_;
+	head_=0;
+	size_=other.size_;
+	capacity_=other.capacity_;
+   	moves_=0;}
+        return *this;
 }
 
 template<typename T>
 QueueCircular<T> &QueueCircular<T>::operator=(QueueCircular &&) noexcept {
-  // TODO: liberar el recurso actual, transferir ownership y vaciar el origen.
-  return *this;
+      if(this!=&other){
+	delete[] data_;
+          this->data_=other.data_;
+	  this->head_=other.head_;
+          this->size_=other.size_;
+          this->capacity_=other.capacity_;
+	  this->moves_=other.moves_;
+  other.data_=nullptr;
+  other.head=0;
+  other.size_=0;
+  other.capacity_=0;
+  other.moves_=0;}
+	return *this;
 }
+
 
 template<typename T>
 QueueCircular<T>::~QueueCircular() {
@@ -79,26 +125,40 @@ QueueCircular<T>::~QueueCircular() {
 }
 
 template<typename T>
-void QueueCircular<T>::push(const T &) {
-  throw std::logic_error("TODO QueueCircular::push(const T&)");
+void QueueCircular<T>::push(const T &x) {
+if(size_==capacity_)
+	grow();
+data_[physical_index(size_)]=x;
+size_++;
 }
 
 template<typename T>
-void QueueCircular<T>::push(T &&) {
-  throw std::logic_error("TODO QueueCircular::push(T&&)");
+void QueueCircular<T>::push(T &&x) {
+	if(size_==capacity_)
+		grow();
+	data_[physical_index(size_)]=x;
+	size_++;
 }
 
 template<typename T>
 void QueueCircular<T>::pop() {
-  throw std::logic_error("TODO QueueCircular::pop");
+if(size_==0)
+	throw out_of_range("Queue vacia");
+head_=(head_+1)%capacity_;
+head--;
 }
 
 template<typename T>
 T &QueueCircular<T>::front() {
-  throw std::logic_error("TODO QueueCircular::front");
+if(size_==0)
+	throw out_of_range("Queue vacia");
+return data_[head_];
 }
 
 template<typename T>
 const T &QueueCircular<T>::front() const {
-  throw std::logic_error("TODO QueueCircular::front const");
+if(size_==0)
+	throw out_of_range("Queue vacia");
+return data_[head_];
 }
+#endif
